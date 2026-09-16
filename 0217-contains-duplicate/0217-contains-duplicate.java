685ed394cce9baf0e46cd735b5e04db2a31b609d
Monika Sri V <monikasri.cb@gmail.com>
2026-09-16 15:16:09 +0530

    Time: 24 ms (28.31%), Space: 93.2 MB (77.31%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int val:nums){
+            set.add(val);
+        }
+
+        if(set.size()!=nums.length){
+            return true;
+        }
+        return false;
+    }
+}
